e29ddb088d611e2a546561157dd8ba10a41c31cd
EnnyMJE <78366762+EnnyMJE@users.noreply.github.com>
2022-12-29 10:46:16 +0100

    NewLocation

---
+++ src/app/components/newlocation/newlocation.component.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {LocationService} from "../../services/location.service";
+import {Location} from "../../entities/location.entities";
+import {formatDate} from "@angular/common";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-newlocation',
+  templateUrl: './newlocation.component.html',
+  styleUrls: ['./newlocation.component.css']
+})
+export class NewlocationComponent implements OnInit {
+  @Input() cliact?: FormGroup;
+  @Output() newLocation = new EventEmitter<Location>();
+  locationFormGroup?: FormGroup;
+  submitted = false;
+  loc?: Location;
+
+  constructor(private fb: FormBuilder, private locationService: LocationService) { }
+
+  ngOnInit(): void {
+    this.locationFormGroup=this.fb.group({
+      dateloc: [formatDate(new Date(), 'yyyy-MM-dd', 'en')],
+      kmtotal:['',Validators.required],
+      cout: ['',Validators.required]
+    });
+  }
+
+  onSaveLocation(): void {
+    this.submitted=true;
+
+    this.locationService.save(this.locationFormGroup?.value, this.cliact?.value).subscribe(data=>{
+      alert('sauvegarde ok');
+      this.loc=data;
+      this.newLocation.emit(data)
+    },
+      err=> {
+      alert(err.headers.get("error"));
+      });
+
+  }
+}
